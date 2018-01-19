155b0024e785ec75ac924e24c6a0ea84a8c877c1
Guido Gloor <haslo@haslo.ch>
2018-01-19 11:43:29 +0100

    Changes for newer autocomplete implementation in jQuery UI
    ...which no longer has an a tag in the .ui-menu-item

--- lib/corner_stones/form/fields/autocomplete_field.rb
+++ lib/corner_stones/form/fields/autocomplete_field.rb
@@ -20,10 +20,10 @@ module CornerStones
           page.execute_script %Q{ $('##{autocomplete_id}').trigger("focus") }
           page.execute_script %Q{ $('##{autocomplete_id}').trigger("keydown") }
           wait_until do
-            result = page.evaluate_script %Q{ $('.ui-menu-item a:contains("#{value}")').size() }
+            result = page.evaluate_script %Q{ $('.ui-menu-item:contains("#{value}")').size() }
             result > 0
           end
-          page.execute_script %Q{ $('.ui-menu-item a:contains("#{value}")').trigger("mouseenter").trigger("click"); }
+          page.execute_script %Q{ $('.ui-menu-item:contains("#{value}")').trigger("mouseenter").trigger("click"); }
         end
 
         def wait_until
